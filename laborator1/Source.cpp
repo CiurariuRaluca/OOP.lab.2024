8a32545179a896b7e62ca1637b47fdacde2d0c95
CiurariuRaluca <161612108+CiurariuRaluca@users.noreply.github.com>
2024-03-06 18:32:44 +0200

    Add files via upload

--- laborator1/Source.cpp
+++ laborator1/Source.cpp
@@ -1,41 +1,20 @@
-#include<iostream>
 #include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+#include "NumberList.h"
 
 using namespace std;
 
-int atoi(char* s) {
-
-	int rez = 0;
-	for (int i = 0; s[i] != '\0'; i++)
-	{
-		rez = rez * 10 + s[i] - '0';
-	}
-	return rez;
-}
-
 int main()
 {
-	FILE* fp;
-	int rezultat = 0;
-	
-	if (fopen_s(&fp, "in.txt", "r") != 0)
-	{
-		printf("eroare.nu am putut deschide fisierul\n");
-	}
-	else
-	{
-		printf("am deschis fisierul cu succes\n");
+	NumberList m;
+	m.Init();
+	m.Add(3);
+	m.Add(4);
+	m.Add(0);
+	m.Sort();
+	m.Print();
 
-		char sir[201];
 
-		while (fgets(sir, 200, fp))
-		{
-			rezultat += atoi(sir);
-		}
-	}
-	printf("suma este:%d \n", rezultat);
-
-	
 	return 0;
+
 }
